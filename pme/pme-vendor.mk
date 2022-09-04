5621b07d02379c67f0af5d514d6f5d366b8511e3
TARKZiM <tom8476oo@gmail.com>
2022-09-04 22:32:49 +0800

    pme: Fix adsp firmware file name derp

--- pme/pme-vendor.mk
+++ pme/pme-vendor.mk
@@ -319,9 +319,9 @@ PRODUCT_COPY_FILES += \
     vendor/htc/pme/proprietary/vendor/firmware/adsp.b04:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b04 \
     vendor/htc/pme/proprietary/vendor/firmware/adsp.b05:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b05 \
     vendor/htc/pme/proprietary/vendor/firmware/adsp.b06:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b06 \
-    vendor/htc/pme/proprietary/vendor/firmware/adsp.b08:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b07 \
-    vendor/htc/pme/proprietary/vendor/firmware/adsp.b09:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b08 \
-    vendor/htc/pme/proprietary/vendor/firmware/adsp.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b09 \
+    vendor/htc/pme/proprietary/vendor/firmware/adsp.b08:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b08 \
+    vendor/htc/pme/proprietary/vendor/firmware/adsp.b09:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b09 \
+    vendor/htc/pme/proprietary/vendor/firmware/adsp.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.mdt \
     vendor/htc/pme/proprietary/vendor/firmware/adspver.cfg:$(TARGET_COPY_OUT_VENDOR)/firmware/adspver.cfg \
     vendor/htc/pme/proprietary/vendor/firmware/BCM4349B1_002.002.014.0051.0061.hcd:$(TARGET_COPY_OUT_VENDOR)/firmware/BCM4349B1_002.002.014.0051.0061.hcd \
     vendor/htc/pme/proprietary/vendor/firmware/cpe_9335.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/cpe_9335.mdt \
